e7313b17e0392261e70a9c37369a5d8a820c90a8
Arthur Pimenta <pimentabraga2@gmail.com>
2020-06-25 17:21:29 -0300

    Router and views with partials

--- web/src/server.js
+++ web/src/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+// Routes
+app.use(routes);
 
 // View engine
 app.set("view engine", "ejs");
@@ -11,7 +15,6 @@ app.use(express.static("public"));
 // Body-Parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 
 // Server
 app.listen(8080, (error) => {
