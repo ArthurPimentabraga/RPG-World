1f354a426ce30b6daa90471f774c15f0bc4ccdc0
Arthur Pimenta <pimentabraga2@gmail.com>
2020-06-25 13:47:07 -0300

    Initial web architecture

---
+++ web/src/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// View engine
+app.set("view engine", "ejs");
+
+// Static
+app.use(express.static("public"));
+
+// Body-Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// Server
+app.listen(8080, (error) => {
+    if (error) {
+        console.log("Error when starding the server! :(");
+    }
+    else{
+        console.log("Server started successfully! :)");
+    }
+})
