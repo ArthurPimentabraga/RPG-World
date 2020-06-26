79d98ea86bbb118ae992e3e234b53aaebc3cfacb
Arthur Pimenta <pimentabraga2@gmail.com>
2020-06-26 16:08:41 -0300

    Created pages 'Register' and 'Login' || Created Controllers

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+// Controllers
+const UserController = require('./controllers/UserController');
+
+// Routes
 router.get('/', (req, res) => {
     res.render('index');
 });
+
+router.get('/login', UserController.login);
+router.get('/register', UserController.register);
 
 module.exports = router;
